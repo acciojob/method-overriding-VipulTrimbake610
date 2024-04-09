7a41920669e2b4caa4f73713b1a5b280c2dfc1c6
VipulTrimbake610 <vipultrimbake610@gmail.com>
2024-04-09 21:15:01 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,6 +14,6 @@ public class Main {
 
     public static void main(String[] args){
         B b = new B();
-        System.out.println( b.meth())it ;
+        System.out.println( b.meth()) ;
     }
 }
