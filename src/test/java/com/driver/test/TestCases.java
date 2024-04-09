03e1359cb264866354f051225e0c3d516ac1a60f
VipulTrimbake610 <vipultrimbake610@gmail.com>
2024-04-09 21:25:16 +0530

    first

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,7 +2,19 @@ package com.driver.test;
 
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
-
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A {
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+}
 public class TestCases {
-    
+    public static void main(String[] args){
+        B b = new B();
+        System.out.println( b.meth()) ;
+    }
 }
